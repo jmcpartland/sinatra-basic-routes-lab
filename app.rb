67330759394c437dcaf571f69fcf25c7aa6d22ad
Joe McPartland <joemcpartland@gmail.com>
2021-04-29 07:53:08 -0600

    commit tree df314dae24d190ad7c2f93691ef0f1806efc3551

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Joe"
+    end 
+
+    get '/hometown' do
+        "My hometown is Denver"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is My Favorite Things"
+    end 
+
 end
